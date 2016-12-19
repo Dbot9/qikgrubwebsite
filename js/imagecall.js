5901d43e0d2b030ef38923e2243e480cb99d3ea2
kriya1 <vasu@kriyative-edge.com>
2016-12-19 18:05:50 +0530

    Add files via upload

---
+++ js/imagecall.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+function funcall6(){document.getElementById("inr1").innerHTML='<img src="images/img/corporate.jpg" alt="" width="528" height="227" />';}
+function funcall7(){document.getElementById("inr1").innerHTML='<img src="images/img/our-values.jpg" alt="" width="528" height="227" />';}
+function funcall8(){document.getElementById("inr1").innerHTML='<img src="images/img/clients.jpg" alt="" width="528" height="227" />';}
+function funcall9(){document.getElementById("inr1").innerHTML='<img src="images/img/testimonials.jpg" alt="" width="528" height="227" />';}
+function funcall10(){document.getElementById("inr1").innerHTML='<img src="images/img/blog.jpg" alt="" width="528" height="227" />';}
+
+function funcall11(){document.getElementById("inr2").innerHTML='<img src="images/img/web-apps.jpg" alt="" width="528" height="227" />';}
+function funcall12(){document.getElementById("inr2").innerHTML='<img src="images/img/mobile_apps.jpg" alt="" width="528" height="227" />';}
+function funcall13(){document.getElementById("inr2").innerHTML='<img src="images/img/web-hosting.jpg" alt="" width="528" height="227" />';}
+function funcall14(){document.getElementById("inr2").innerHTML='<img src="images/img/internet-marketing.jpg" alt="" width="528" height="227" />';}
+function funcall15(){document.getElementById("inr2").innerHTML='<img src="images/img/omd.jpg" alt="" width="528" height="227" />';}
+
+function funcall16(){document.getElementById("inr3").innerHTML='<img src="images/img/mobile_apps.jpg" alt="" width="528" height="227" />';}
+function funcall17(){document.getElementById("inr3").innerHTML='<img src="images/img/web-hosting.jpg" alt="" width="528" height="227" />';}
+function funcall18(){document.getElementById("inr3").innerHTML='<img src="images/img/internet-marketing.jpg" alt="" width="528" height="227" />';}
